ea1b728c729c5c429f0373c6879d65bcee59fb4d
Nick Rupp <nickrupp95@gmail.com>
2017-04-29 19:03:14 +0100

    Add data import and graph plotting

---
+++ classification.py
@@ -0,0 +1,45 @@
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+def load_dataset():
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    dataset = pandas.read_csv(url, names=names)
+    return dataset
+
+
+def data_summary(dataset):
+    print(dataset.describe())
+
+def show_box_and_whisker_plot(dataset):
+    dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+    plt.show()
+
+def show_histogram(dataset):
+    dataset.hist()
+    plt.show()
+
+
+def show_multivariate_scatter_plot(dataset):
+    scatter_matrix(dataset)
+    plt.show()
+
+def create_validation(dataset):
+    array = dataset.values
+    X = array[:,0:4]
+    Y = array[:,4]
+    validation_size = 0.20
+    seed = 7
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
